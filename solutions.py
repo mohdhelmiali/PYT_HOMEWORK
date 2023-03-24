c56b65cf6981b23ce91d26e62049588851e61df6
helmixeemx <helmi@xeemx.com>
2023-03-24 11:42:11 +0800

    add new variable homework

---
+++ solutions.py
@@ -0,0 +1,62 @@
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total)
+
+beeps = 0
+boops = 5
+total2 = beeps + boops
+print(total2)
+
+beeps = 9
+boops = 3
+total3 = beeps + boops
+print(total3)
+
+beeps = 4
+boops = 8
+total4 = beeps + boops
+print(total4)
+
+beeps = 10
+boops = 5
+total5 = beeps + boops
+print(total5)
+
+beeps = 11
+boops = 12
+total6 = beeps + boops
+print(total6)
+
+beeps = 5
+boops = 5
+total7 = beeps + boops
+print(total7)
+
+beeps = 1
+boops = 17
+total8 = beeps + boops
+print(total8)
+
+beeps = 5
+boops = 7
+total9 = beeps + boops
+print(total9)
+
+beeps = 4
+boops = 0
+total10 = beeps + boops
+print(total10)
+
+total1 = "H"
+total2 = "E"
+total3 = "L"
+total4 = "L"
+total5 = "O"
+total6 = "W"
+total7 = "J"
+total8 = "R"
+total9 = "L"
+total10 = "D"
+
+print(f"{total1}{total2}{total3}{total4}{total5} {total6}{total7}{total8}{total9}{total10}")
